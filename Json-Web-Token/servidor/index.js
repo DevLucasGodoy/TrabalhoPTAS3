ffda0e63682f3111701c2cab47042b4c2ef44f51
DevLucasGodoy <lucas.moraes.godoy@gmail.com>
2022-09-21 08:40:11 -0400

    first commit

---
+++ Json-Web-Token/servidor/index.js
@@ -0,0 +1,74 @@
+// JWT
+require("dotenv-safe").config();
+const jwt = require('jsonwebtoken');
+var { expressjwt: expressJWT } = require("express-jwt");
+const cors = require('cors');
+
+var cookieParser = require('cookie-parser')
+
+const express = require('express');
+const { usuario } = require('./models');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+app.use(cookieParser());
+app.use(
+  expressJWT({
+    secret: process.env.SECRET,
+    algorithms: ["HS256"],
+    getToken: req => req.cookies.token
+  }).unless({ path: ["/", "/autenticar", "/logar", "/deslogar", "/cadastrar", "/usuarios"] })
+);
+
+app.get('/autenticar', async function(req, res){
+  res.render('autenticar');
+})
+
+app.get('/', async function(req, res){
+  res.render("home")
+})
+
+app.get('/cadastrar', async function(req, res){
+  res.render('cadastrar')
+})
+
+app.post('/cadastrar', async function(req, res){
+  const usuario_ = await usuario.create(req.body)
+  res.json(usuario_)
+})
+
+app.get('/usuarios', async function(req, res){
+  const usuarios = await usuario.findAll();
+  res.render("cadastrar", { usuarios });
+})
+
+app.post('/logar', (req, res) => {
+  if(req.body.user === 'admin' && req.body.password === 'admin'){
+    const id = 1;
+    const token = jwt.sign({ id }, process.env.SECRET, {
+      expiresIn: 3000 // expires in 50min
+    });
+
+    res.cookie('token', token, { httpOnly: true });
+    return res.json({ auth: true, token: token });
+  }
+
+  res.status(500).json({message: 'Login inválido!'});
+})
+
+app.post('/deslogar', function(req, res) {
+  res.cookie('token', null, { httpOnly: true });
+  res.json({deslogado: true})
+})
+
+app.listen(3000, function() {
+  console.log('Execução na porta 3000!')
+});
